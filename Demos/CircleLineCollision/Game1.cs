09710053c8188ed3e80f3e19175df64447fbc37f
Brian Hearn <brian@brianhearn.com>
2018-10-14 22:41:55 +0100

    Line intersecting circle collision

---
+++ Demos/CircleLineCollision/Game1.cs
@@ -0,0 +1,114 @@
+﻿using Hearn.MonoGame.Geometry;
+using Hearn.MonoGame.Widgets;
+using LilyPath;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CircleLineCollision
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        DrawBatch drawBatch;
+
+        Circle _circle;
+        Line _line;
+
+        Brush _brush;
+
+        DraggableCircle[] _dragPoints;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+        
+        protected override void Initialize()
+        {
+            drawBatch = new DrawBatch(GraphicsDevice);
+
+            const int Margin = 50;
+            const int DragCircleRadius = 10;
+
+            var x = GraphicsDevice.Viewport.Width - Margin;
+            var y = GraphicsDevice.Viewport.Height - Margin;
+
+            _line = new Line() { Start = new Vector2(Margin, Margin), End = new Vector2(x, y) };
+
+            _dragPoints = new DraggableCircle[4];
+
+            _dragPoints[0] = new DraggableCircle()
+            {
+                Location = _line.Start,
+                Radius = DragCircleRadius,
+                OnDrag = v => { _line.Start = v; }
+            };
+
+            _dragPoints[1] = new DraggableCircle()
+            {
+                Location = _line.End,
+                Radius = DragCircleRadius,
+                OnDrag = v => { _line.End = v; }
+            };
+
+            _circle = new Circle() { Radius = 50 };
+
+            base.Initialize();
+        }
+        
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+        
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            for (var i = 0; i < 2; i++)
+            {
+                _dragPoints[i].Update();
+            }
+
+            var mouseState = Mouse.GetState();
+            _circle.Location = new Vector2(mouseState.X, mouseState.Y);
+
+            _brush = Brush.White;
+            if (_circle.Collides(_line))
+            {
+                _brush = Brush.Red;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            drawBatch.Begin();
+
+            drawBatch.FillCircle(_brush, _circle.Location, _circle.Radius);
+
+            drawBatch.DrawLine(Pen.Black, _line.Start, _line.End);
+
+            for (var i = 0; i < 2; i++)
+            {
+                drawBatch.FillCircle(Brush.Green, _dragPoints[i].Location, _dragPoints[i].Radius);
+            }
+
+            drawBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
--- Hearn.MonoGame/Geometry/Circle.cs
+++ Hearn.MonoGame/Geometry/Circle.cs
@@ -31,5 +31,49 @@ namespace Hearn.MonoGame.Geometry
             return Vector2Ex.Distance(v, Location) <= Radius;
         }
 
+        public bool Collides(Line l)
+        {
+
+            //http://mathworld.wolfram.com/Circle-LineIntersection.html
+
+            var x1 = l.Start.X - Location.X;
+            var y1 = l.Start.Y - Location.Y;
+
+            var x2 = l.End.X - Location.X;
+            var y2 = l.End.Y - Location.Y;
+
+            var dx = x2 - x1;
+            var dy = y2 - y1;
+            var dr = Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2));
+            var d = (x1 * y2) - (x2 * y1);
+
+            var incidence = (Math.Pow(Radius, 2) * Math.Pow(dr, 2)) - Math.Pow(d, 2);
+
+            if (incidence >= 0)
+            {
+
+                var ix1 = ((d * dy) + ((Math.Sign(dy) * dx) * Math.Sqrt(incidence))) / Math.Pow(dr, 2);
+                var iy1 = ((-d * dx) + (Math.Abs(dy) * Math.Sqrt(incidence))) / Math.Pow(dr, 2);
+
+                if (ix1 >= Math.Min(x1, x2) && ix1 <= Math.Max(x1, x2)
+                    && iy1 >= Math.Min(y1, y2) && iy1 <= Math.Max(y1, y2))
+                {
+                    return true;
+                }
+
+                var ix2 = ((d * dy) - ((Math.Sign(dy) * dx) * Math.Sqrt(incidence))) / Math.Pow(dr, 2);
+                var iy2 = ((-d * dx) - (Math.Abs(dy) * Math.Sqrt(incidence))) / Math.Pow(dr, 2);
+
+                if (ix2 >= Math.Min(x1, x2) && ix2 <= Math.Max(x1, x2)
+                    && iy2 >= Math.Min(y1, y2) && iy2 <= Math.Max(y1, y2))
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+
+        }
     }
 }
