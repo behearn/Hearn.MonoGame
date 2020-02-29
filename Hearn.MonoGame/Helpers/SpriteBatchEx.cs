33d6712ff54c79883093f0286d301c5f71cd8f82
Brian Hearn <brian@brianhearn.com>
2020-02-29 21:06:51 +0000

    Added SpriteBatch Draw Extension Methods

---
+++ Hearn.MonoGame/Helpers/SpriteBatchEx.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hearn.MonoGame.Helpers
+{
+    public static class SpriteBatchEx
+    {
+        public static void Draw(this SpriteBatch spriteBatch, Texture2D texture, Vector2 position, Vector2 scale, Vector2 origin)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, 0f, origin, scale, SpriteEffects.None, 0f);
+        }
+
+        public static void Draw(this SpriteBatch spriteBatch, Texture2D texture, Vector2 position, Rectangle sourceRectangle, Vector2 scale, Vector2 origin)
+        {
+            spriteBatch.Draw(texture, position, sourceRectangle, Color.White, 0f, origin, scale, SpriteEffects.None, 0f);
+        }
+
+    }
+}
